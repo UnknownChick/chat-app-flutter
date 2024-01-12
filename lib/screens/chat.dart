184e8700080c6a3c4fdfff377862854b25ae7fd3
UnknownChick <ferreira.alexandre.2000@outlook.com>
2024-01-12 14:24:52 +0100

    feat(push notification): requesting permission and getting an acces token

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,9 +2,31 @@ import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance; // permet de créer une instance de FirebaseMessaging
+
+    await fcm.requestPermission(); // permet de demander la permission pour les notifications
+
+    final token = await fcm.getToken(); // permet de récupérer le token de l'utilisateur
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
